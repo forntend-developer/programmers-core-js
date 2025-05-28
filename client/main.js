47eecbe0d675790db5a2687dda4df61df5b9005d
forntend-developer <ejdeveloper0305@gmail.com>
2025-05-28 15:20:27 +0900

    first commit

---
+++ client/main.js
@@ -0,0 +1,5 @@
+console.log(navigator.language);
+// console.log(process.env.LANG);
+
+/* 여기서  */
+const a = 10;
---
+++ server/index.js
@@ -0,0 +1,12 @@
+// console.log(navigator.language);
+
+const server = require("live-server");
+
+const params = {
+  host: "localhost",
+  port: 3000,
+  open: false,
+  root: "./client",
+};
+
+server.start(params);
