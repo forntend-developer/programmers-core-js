c07eef96915905a27d7fafc9675de08205a2d0e0
forntend-developer <ejdeveloper0305@gmail.com>
2025-06-20 11:40:01 +0900

    수업내용 업로드

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -1,5 +1,7 @@
 import { getNode } from '../dom/getNode.js';
+import { insertLast } from '../dom/insert.js';
 import { isNumber, isObject } from './type.js';
+import { xhrPromise} from './xhr.js';
 
 // callback
 
@@ -25,7 +27,6 @@ const second = getNode('.second');
 
 // 내가 이거 끝나면 꼭 너한테 알려줄게 약속할게...
 
-
 // promise
 
 /*
@@ -38,77 +39,143 @@ Promise를 사용하는 이유?
 
  */
 
-
 // object mixin
 
 const defaultOptions = {
-  shouldRejected : false,
-  data : '성공',
-  errorMessage : '오류 발생!',
-  timeout : 1000, 
-}
+  shouldRejected: false,
+  data: '성공',
+  errorMessage: '오류 발생!',
+  timeout: 1000,
+};
 
-function delayP(options){
-
+export function delayP(options) {
   //console.log(options);
   // const config = {...defaultOptions, ...options};
-  let config = {...defaultOptions}
+  let config = { ...defaultOptions };
 
   // option이 숫자 일때 isNumber()
-  if(isNumber(options)){
+  if (isNumber(options)) {
     config.timeout = options;
   }
 
   // option이 객체 일때 isObject()
-  if(isObject(options)){
-    config = {...defaultOptions,...options}
-
+  if (isObject(options)) {
+    config = { ...defaultOptions, ...options };
   }
-  
+
   //console.log(config);
 
-  
-  const {shouldRejected, timeout, errorMessage, data} = config;
+  const { shouldRejected, timeout, errorMessage, data } = config;
 
-  return new Promise((resolve,reject) => {
-
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      
-      if(!shouldRejected){
+      if (!shouldRejected) {
         resolve(data);
-      } else{
-        reject({message: errorMessage});
+      } else {
+        reject({ message: errorMessage });
       }
-    
     }, timeout);
-  })
+  });
 }
 
 //console.log( delayP() );
 
-delayP(1000)
+// delayP(1000);
 
-delayP({
-  // data: '성공!!',
-  // shouldRejected: false,
-  // timeout: 1000,
-  errorMessage:'오류 발생!'
-}) // [[promise object]]
-.then(()=>{
+// delayP({
+//   // data: '성공!!',
+//   // shouldRejected: false,
+//   // timeout: 1000,
+//   errorMessage: '오류 발생!',
+// }) // [[promise object]]
+//   .then(() => {
+//     first.style.top = '-100px';
+//     second.style.top = '100px';
 
-  first.style.top = '-100px';
-  second.style.top = '100px';
+//     return delayP();
+//   })
+//   .then(() => {
+//     first.style.transform = 'rotate(360deg)';
+//     second.style.transform = 'rotate(-360deg)';
 
-  return delayP();
-})
-.then(()=>{
-  first.style.transform = 'rotate(360deg)';
-  second.style.transform = 'rotate(-360deg)';
+//     return delayP();
+//   })
+//   .then(() => {
+//     first.style.top = 0;
+//     second.style.top = 0;
+//   });
 
-  return delayP();
-})
-.then(()=>{
-  first.style.top = 0;
-  second.style.top = 0;
+// const p = new Promise((resolve, reject)=>{    // 콜백함수 들어감
+// })
+// p.then((result)=>{  // then은 무조건프로미스 객체 내보냄
+//  return p ()
+// })
 
-})
+//
+
+
+
+// async await  
+// async : 무조건!! Promise 객체 내보냄
+// await : 코드 실행 흐름 제어
+//         result의 값을 꺼낼 수 있다.
+
+async function f(){
+  return 10;
+}
+
+
+const a = f();
+
+const data = await a;   // top-level await
+
+
+//IIAFE  -> top-level await 사용하기 위해 이렇게 쓰는 경우가 있음, 즉시 실행
+(async()=>{
+  const a = await f();
+})()
+
+
+async function delayA() {
+  return new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      resolve('성공')
+    }, 2000);
+  })
+}
+
+// const result =  await delayA();
+
+// console.log(result);
+
+
+
+async function 라면끓이기() {
+
+  const a = await delayP({data:'물'})
+  console.log(a);
+
+  const b = await delayP({data:'불켜기'})
+  console.log(b);
+
+  const c = await delayP({data:'스프'})
+  console.log(c);
+
+  
+  console.log('면');
+  console.log('계란');
+  console.log('먹기');
+  
+}
+
+//라면끓이기();
+
+
+async function getData() {
+  const data = await xhrPromise.get('https://pokeapi.co/api/v2/pokemon/1');
+  const src = data.sprites.other.showdown['front_default'];
+  
+  insertLast(document.body,`<img src="${src}" alt="" />`)
+}
+
+getData();
--- client/lib/utils/xhr.js
+++ client/lib/utils/xhr.js
@@ -1,7 +1,5 @@
 
-
-
-const END_POINT = 'https://jsonplaceholder.typicode.com/users';
+export const END_POINT = 'http://localhost:3000/posts';
 
 
 /*
@@ -138,11 +136,11 @@ xhr.get = (url, success, fail) => {  // 객체안에 함수를 정의 -> 메소�
 }
 
 
-xhr.get(
-  END_POINT,
-  (data) => console.log(data),
-  () => {}
-)
+// xhr.get(
+//   END_POINT,
+//   (data) => console.log(data),
+//   () => {}
+// )
 
 
 xhr.post = (url, body, success, fail) => {
@@ -196,3 +194,70 @@ xhr.patch = (url,body,success,fail) => {
 //   () => {},
 //   () => {},
 // )
+
+
+
+
+const defaultOptions = {
+  method:'GET',
+  url:'',
+  body:null,
+  errorMessage:'서버와의 통신이 원활하지 않습니다.',
+  headers: {
+    'Content-Type':'application/json',
+    'Access-Control-Allow-Origin':'*'
+  }
+}
+
+export function xhrPromise(options = {}){
+
+  const {method,url,headers,body,errorMessage:message} = {
+    ...defaultOptions,
+    ...options,
+    headers:{
+      ...defaultOptions.headers,
+      ...options.headers
+    }
+  };
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open(method,url);
+
+  if(!(method === 'DELETE')){
+    Object.entries(headers).forEach(([k,v])=>{
+      xhr.setRequestHeader(k,v);
+    })
+  }
+
+  xhr.send(body ? JSON.stringify(body) : null);
+
+  return new Promise((resolve, reject) => {
+      xhr.addEventListener('readystatechange',()=>{
+      const { readyState, status, response } = xhr;
+      if(readyState === 4){
+        if(status >= 200 && status < 400){
+          resolve(JSON.parse(response));
+        }else{
+          reject({message});
+        }
+      }
+    })
+  })
+}
+
+
+// xhrPromise({ url:END_POINT })
+// .then((res)=>{
+//   console.log( res );
+// },
+// (err)=>{
+//    console.log( err );
+// })
+
+
+xhrPromise.get = url => xhrPromise({url});
+xhrPromise.post = (url,body) => xhrPromise({url,body,method:'POST'});
+xhrPromise.put = (url,body) =>  xhrPromise({url,body,method:'PUT'});
+xhrPromise.patch = (url,body) =>  xhrPromise({url,body,method:'PATCH'});
+xhrPromise.delete = url => xhrPromise({url,method:'DELETE'});
